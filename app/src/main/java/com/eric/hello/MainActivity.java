76ef9e471c915ffe758a34694b417997be9904b1
hadesyi <leesk55@gmail.com>
2019-04-19 17:33:18 +0900

    버튼을 눌렀을 때 메시지가 나타나게 하기

--- app/src/main/java/com/eric/hello/MainActivity.java
+++ app/src/main/java/com/eric/hello/MainActivity.java
@@ -1,7 +1,9 @@
 package com.eric.hello;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(this, "확인1 버튼이 눌렸어요.", Toast.LENGTH_LONG).show();
     }
 }
